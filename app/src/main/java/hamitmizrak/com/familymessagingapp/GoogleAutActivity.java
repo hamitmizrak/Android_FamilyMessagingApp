11377763b7d55e05dc984142cabb8aa925c72bcf
hamitmizrak <hamitmizrak@gmail.com>
2022-08-20 22:28:23 +0300

    ana main

---
+++ app/src/main/java/hamitmizrak/com/familymessagingapp/GoogleAutActivity.java
@@ -0,0 +1,13 @@
+package hamitmizrak.com.familymessagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleAutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_aut);
+    }
+}
