cf6c36287d0edb923ee13329c7216ed844db921c
hamitmizrak <hamitmizrak@gmail.com>
2022-08-21 20:47:24 +0300

    mainActivity google sign in

--- app/src/main/java/hamitmizrak/com/familymessagingapp/MainActivity.java
+++ app/src/main/java/hamitmizrak/com/familymessagingapp/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     SignInButton buttonGoogleIndent;
 
     //user email ve password
-    String userEmailAddress,userPassword;
+    String userEmailAddress, userPassword;
 
     //Firebase işlerimleri
     private FirebaseAuth firebaseAuth;
@@ -62,33 +68,74 @@ public class MainActivity extends AppCompatActivity {
         //onCreate Start Codes
 
         //id Almak
-         editTextLoginMailAddress=findViewById(R.id.editTextLoginMailAddress);
-         editTextLoginPassword=findViewById(R.id.editTextLoginPassword);
-         buttonLogin=findViewById(R.id.buttonLogin);
+        editTextLoginMailAddress = findViewById(R.id.editTextLoginMailAddress);
+        editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
 
-         //google id almak
-        buttonGoogleIndent=findViewById(R.id.buttonGoogleIndent);
+        //google id almak
+        buttonGoogleIndent = findViewById(R.id.buttonGoogleIndent);
 
         //Firebase Instance
-        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         //Kullanıcının verilerini almak
-        userEmailAddress=editTextLoginMailAddress.getText().toString();
-        userPassword=editTextLoginPassword.getText().toString();
+        userEmailAddress = editTextLoginMailAddress.getText().toString();
+        userPassword = editTextLoginPassword.getText().toString();
 
         //Kullanıcı sisteme giriş/çıkış yapmış mı ?
-        authStateListener=new FirebaseAuth.AuthStateListener() {
+        //eğer sistemde Kullanıcı varsa Admin sayfasına yönlendir
+        authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 //kullanıcı bilgisini almak
-                firebaseUser=firebaseAuth.getCurrentUser();
-                if(firebaseUser!=null){
-                    Intent adminIndent=new Intent(getApplicationContext(),AdminActivty.class);
+                firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Intent adminIndent = new Intent(getApplicationContext(), AdminActivty.class);
+                    //Toast ==>  @string veri almak istiyorsak getString(R.string.stringAdi)
+                    Toast.makeText(MainActivity.this, getString(R.string.admin_redirect), Toast.LENGTH_SHORT).show();
                     startActivity(adminIndent);
                 }// end id
             }// end onAuthStateChanged
         };//end authStateListener
 
+        // +++++ +++ //
+        //buttonLogin tıklandığında
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //butona tıkladığımda inputtlardan aldığım verilerle sisteme giriş yapmak
+                // addOnCompleteListener: sisteme giriş dinlemek
+                firebaseAuth.signInWithEmailAndPassword(userEmailAddress, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    //eğer sisteme giriş başarılıysa admin page yönlendirsin
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Intent adminIndent = new Intent(getApplicationContext(), AdminActivty.class);
+                        //Toast ==>  @string veri almak istiyorsak getString(R.string.stringAdi)
+                        Toast.makeText(MainActivity.this, getString(R.string.admin_redirect), Toast.LENGTH_SHORT).show();
+                        startActivity(adminIndent);
+                    }
+                    //eğer sisteme giriş yaparken herhangi bir hata alırsam examp: internet yok,kullanıcı yok
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, getString(R.string.login_faile), Toast.LENGTH_SHORT).show();
+                    }//end onFailure
+                }); //end addOnFailureListener
+            }//end onClick
+        });//end setOnClickListener
+
+        // +++++ +++ //
+        //Google Button
+        buttonGoogleIndent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Google Sayfasına gitmek
+                Intent googleIndent = new Intent(getApplicationContext(), GoogleAutActivity.class);
+                //Toast ==>  @string veri almak istiyorsak getString(R.string.stringAdi)
+                Toast.makeText(MainActivity.this, getString(R.string.sigin_google), Toast.LENGTH_SHORT).show();
+                startActivity(googleIndent);
+            }//end onClick
+        }); // end buttonGoogleIndent
 
         //onCreate End Codes
     }//end onCreate
