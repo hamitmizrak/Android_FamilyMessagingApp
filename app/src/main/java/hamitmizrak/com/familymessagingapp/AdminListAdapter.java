87209f0bcec2e852e3b2eb7afdcd3ae3a6983ce9
hamitmizrak <hamitmizrak@gmail.com>
2022-09-18 21:53:21 +0300

    revize resim

--- app/src/main/java/hamitmizrak/com/familymessagingapp/AdminListAdapter.java
+++ app/src/main/java/hamitmizrak/com/familymessagingapp/AdminListAdapter.java
@@ -53,7 +53,7 @@ public class AdminListAdapter extends BaseAdapter {
 
         //Resim
         Picasso.with(context).setLoggingEnabled(true);
-        Picasso.with(context).load(listViewAdapters.get(i).getImage()).placeholder(R.drawable.avatar2).resize(30,40).into(circleImageView);
+        Picasso.with(context).load(listViewAdapters.get(i).getImage()).placeholder(R.drawable.avatar2).resize(100,100).into(circleImageView);
         return view1;
     }
 }
